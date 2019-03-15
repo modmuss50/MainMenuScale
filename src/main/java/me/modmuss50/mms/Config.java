0e4eee42acf4d8e0421c2ace2002fffc683c4743
modmuss50 <modmuss50@gmail.com>
2019-03-15 14:39:40 +0000

    Added high res mode

--- src/main/java/me/modmuss50/mms/Config.java
+++ src/main/java/me/modmuss50/mms/Config.java
@@ -9,5 +9,10 @@ public class Config {
 	@ConfigRegistry(comment = "The gui scale for the main menu (0 Auto, 1 small, 2 normal, 3 large)")
 	public static int GUI_SCALE = 0;
 
+	@ConfigRegistry(comment = "-1 disabled, the min height that should be used to see if the high res scale should be used")
+	public static int HIGH_RES_HEIGHT = -1;
+
+	@ConfigRegistry(comment = "The gui scale for the main menu with high res (0 Auto, 1 small, 2 normal, 3 large)")
+	public static int HIGH_RES_SCALE = 0;
 
 }
--- src/main/java/me/modmuss50/mms/MainMenuScale.java
+++ src/main/java/me/modmuss50/mms/MainMenuScale.java
@@ -1,5 +1,6 @@
 package me.modmuss50.mms;
 
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -9,6 +10,7 @@ import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.Display;
 
 @Mod(modid = "mainmenuscale", name = "Main Menu Scale", clientSideOnly = true, dependencies = "required-after:reborncore")
 public class MainMenuScale {
@@ -20,28 +22,43 @@ public class MainMenuScale {
 
 	private boolean isMenuOpen = false;
 	private int guiScale = -1;
+	private int lastHeight = 0;
+
+	private strictfp float
 
 	@SubscribeEvent
 	public void initGuiPre(GuiScreenEvent.InitGuiEvent.Pre event){
 		if(event.getGui() instanceof GuiMainMenu || event.getGui().getClass().getName().equals("lumien.custommainmenu.gui.GuiCustom")){
-			openMainMenu();
+			openMainMenu(event.getGui());
 		} else {
 			closeMainMenu();
 		}
 	}
 
-	public void openMainMenu(){
-		if(isMenuOpen){
+	public void openMainMenu(GuiScreen gui){
+		int height = Display.getHeight();
+		if(isMenuOpen && lastHeight == height){
 			return;
 		}
+		lastHeight = height;
+		if(!isMenuOpen){
+			guiScale = Minecraft.getMinecraft().gameSettings.guiScale;
+		}
 		isMenuOpen = true;
-		guiScale = Minecraft.getMinecraft().gameSettings.guiScale;
-		Minecraft.getMinecraft().gameSettings.guiScale = Config.GUI_SCALE;
+
+		int scale = Config.GUI_SCALE;
+		if(Config.HIGH_RES_HEIGHT != -1){
+			if(height >= Config.HIGH_RES_HEIGHT){
+				scale = Config.HIGH_RES_SCALE;
+			}
+		}
+		Minecraft.getMinecraft().gameSettings.guiScale = scale;
 		updateRes();
 	}
 
 	public void closeMainMenu(){
 		if(!isMenuOpen){
+			lastHeight = -1;
 			return;
 		}
 		isMenuOpen = false;
